56eb290a56ac4706942be232a8361a56a6860020
Juan Sánchez <jsmontesinos@gmail.com>
2016-05-28 18:06:48 +0200

    First commit

--- src/main/java/com/jsmontesinos/kata/bowling/App.java
+++ src/main/java/com/jsmontesinos/kata/bowling/App.java
@@ -10,4 +10,50 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    
+    private boolean isNumeric(String string) {
+    	try {
+    		Integer.valueOf(string);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+    	
+    	return true;
+    }
+
+	public int calculateScore(String string) {
+		int score = 0;
+		
+		int maxLength = Math.min(string.length(), 20);
+		for (int i = 0; i < maxLength; i++){
+			String currentThrow = string.substring(i, i + 1);
+			if (isNumeric(currentThrow))
+				score += Integer.valueOf(currentThrow);
+			else if(isSpare(currentThrow)) {
+				String before = string.substring(i - 1, i);
+				if (isNumeric(before)) {
+					score += 10 - Integer.valueOf(before);
+				} 
+				else {
+					score += 10;
+				}
+				String after = string.substring(i + 1, i + 2);
+				if (isNumeric(after))
+					score +=  Integer.valueOf(after);
+			}
+			else if (isStrike(currentThrow)) {
+				score += (300/12);
+			}
+		}
+		
+		return score;
+	}
+
+	private boolean isStrike(String currentThrow) {
+		return currentThrow.equals("X");
+	}
+
+	private boolean isSpare(String currentThrow) {
+		return currentThrow.equals("/");
+	}
 }
--- src/test/java/com/jsmontesinos/kata/bowling/AppTest.java
+++ src/test/java/com/jsmontesinos/kata/bowling/AppTest.java
@@ -1,6 +1,7 @@
 package com.jsmontesinos.kata.bowling;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -9,8 +10,66 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest {
+	
+	
+	
 	@Test
-	public void testName() throws Exception {
-		assertTrue(true);
+	public void should_return_0_when_no_bowls_in_game(){
+		App app = new App();
+		int score = app.calculateScore("--------------------");
+		
+		assertEquals(0, score);
 	}
+	
+	@Test
+	public void should_return_10_when_one_bowl_per_turn(){
+		App app = new App();
+		int score = app.calculateScore("1-1-1-1-1-1-1-1-1-1-");
+		
+		assertEquals(10, score);
+	}
+	
+	@Test
+	public void should_return_50_when_one_and_nine_bowls(){
+		App app = new App();
+		int score = app.calculateScore("1-9-1-9-1-9-1-9-1-9-");
+		
+		assertEquals(50, score);
+	}
+	
+	@Test
+	public void should_return_80_when_five_and_three(){
+		App app = new App();
+		int score = app.calculateScore("53535353535353535353");
+		
+		assertEquals(80, score);
+	}
+	
+	@Test
+	public void should_return_100_when_empty_and_spare(){
+		App app = new App();
+		int score = app.calculateScore("-/-/-/-/-/-/-/-/-/-/-");
+		
+		assertEquals(100, score);
+	}
+	
+	@Test
+	public void should_return_300_when_all_strikes(){
+		App app = new App();
+		int score = app.calculateScore("XXXXXXXXXXXX");
+		
+		assertEquals(300, score);
+	}
+	
+	@Test
+	public void should_return_150_when_five_and_spare(){
+		App app = new App();
+		int score = app.calculateScore("5/5/5/5/5/5/5/5/5/5/5");
+		
+		assertEquals(150, score);
+	}
+	
+	
+	
+	
 }
